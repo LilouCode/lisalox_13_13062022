cf0dd15b2d2f6619c53ac2258ea0992dab59e319
LilouCode <lisa.music@outlook.com>
2022-06-23 19:14:05 +0200

    build: add routes

---
+++ src/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import User from "./pages/User";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App/>}> 
+            <Route path="/" element={<Home/>}/>
+            <Route path="/signIn" element={<SignIn/>}/>
+            <Route path="/user" element={<User/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
