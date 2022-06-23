24742bfd6fd0335a62ec78d052fc298a832ee905
LilouCode <lisa.music@outlook.com>
2022-06-23 19:15:53 +0200

    feat: creat pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Banner from "../components/Banner";
+import Cards from "../components/Cards";
+
+function Home(){
+    return(
+        <main>
+            <Banner/>
+            <Cards/>
+        </main>
+    )
+} export default Home
