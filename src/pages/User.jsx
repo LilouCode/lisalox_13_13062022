e533712cce66abf81f8d08c255e66a269015eec7
LilouCode <lisa.music@outlook.com>
2022-06-24 07:39:06 +0200

    feat: create user page

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,5 +1,33 @@
-function User(){
-    return(
-        <div></div>
-    )
-} export default User
+import Account from "../components/Account";
+
+function User() {
+  return (
+    <main className="main bg-dark">
+      <div class="header">
+        <h1>
+          Welcome back
+          <br />
+          Tony Jarvis!
+        </h1>
+        <button class="edit-button">Edit Name</button>
+      </div>
+      <h2 class="sr-only">Accounts</h2>
+      <Account
+        title="Argent Bank Checking (x8349)"
+        amount={"$2,082.79"}
+        description={"Available Balance"}
+      />
+      <Account
+        title="Argent Bank Checking (x8349)"
+        amount={"$2,082.79"}
+        description={"Available Balance"}
+      />
+      <Account
+        title="Argent Bank Checking (x8349)"
+        amount={"$2,082.79"}
+        description={"Available Balance"}
+      />
+    </main>
+  );
+}
+export default User;
