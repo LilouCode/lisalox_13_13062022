9e660ee714739868aff72f7972f4f83fc6b0e350
LilouCode <lisa.music@outlook.com>
2022-07-03 21:12:06 +0200

    feat: create app and index files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-// import React from 'react';
+import React from 'react';
 import { Outlet } from "react-router-dom";
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './_components/Header';
+import Footer from './_components/Footer';
 function App() {
+  
   return (
     <div className="App">
-      <body>
       <Header/>
       <Outlet/>
       <Footer/>
-      </body>
     </div>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
+import { store } from "./store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
-import Home from "./pages/Home";
-import SignIn from "./pages/SignIn";
-import User from "./pages/User";
+import Home from "./_pages/Home";
+import SignIn from "./_pages/SignIn";
+import User from "./_pages/User";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -18,7 +18,7 @@ root.render(
         <Routes>
           <Route path="/" element={<App/>}> 
             <Route path="/" element={<Home/>}/>
-            <Route path="/signIn" element={<SignIn/>}/>
+            <Route path="/login" element={<SignIn/>}/>
             <Route path="/user" element={<User/>}/>
           </Route>
         </Routes>
