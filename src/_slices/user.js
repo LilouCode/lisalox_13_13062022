1f2281cb8f876cff8d2ea2c011e8bf618085fd77
LilouCode <lisa.music@outlook.com>
2022-07-09 09:00:24 +0200

    feat: create user slice

---
+++ src/_slices/user.js
@@ -0,0 +1,45 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  token: "",
+  data: {},
+  isEditing: false,
+  wantToBeRemembered: false,
+};
+
+export const editNameSlice = createSlice({
+  name: "user",
+  initialState: initialState,
+  reducers: {
+    toggleEdition: (state) => {
+      state.isEditing = !state.isEditing;
+      console.log(state);
+    },
+    toggleRemember: (state) => {
+      state.wantToBeRemembered = !state.wantToBeRemembered;
+      console.log(state)
+    },
+    downloadUserInfos: (state, { payload }) => {
+      state.data = payload;
+    },
+    deleteUserInfos: (state) => {
+      state.data = {};
+    },
+    chargeToken: (state, { payload }) => {
+      state.token = payload;
+    },
+    deleteToken: (state) => {
+      state.token = "";
+    },
+  },
+});
+const { reducer, actions } = editNameSlice;
+export const {
+  toggleEdition,
+  toggleRemember,
+  downloadUserInfos,
+  deleteUserInfos,
+  chargeToken,
+  deleteToken,
+} = actions;
+export default reducer;
