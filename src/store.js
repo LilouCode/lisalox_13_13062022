897801a3556d060edac27a5ccdcb0d7108f24f6f
LilouCode <lisa.music@outlook.com>
2022-07-09 09:02:50 +0200

    feat: update store with user slice

--- src/store.js
+++ src/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from "./_slices/auth";
 import messageReducer from "./_slices/message";
+import userReducer from './_slices/user';
 
 const reducer = {
   auth: authReducer,
-  message: messageReducer
+  message: messageReducer,
+  user: userReducer,
+  
 }
 export const store = configureStore({
   reducer: reducer,
