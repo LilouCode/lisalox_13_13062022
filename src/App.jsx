fb4324e1be25a1a56011ade1610f558dd7d2e6ed
LilouCode <lisa.music@outlook.com>
2022-07-09 09:01:16 +0200

    refactor: user service

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 import Header from './_components/Header';
 import Footer from './_components/Footer';
 function App() {
-  
   return (
     <div className="App">
       <Header/>
--- src/_services/user.service.js
+++ src/_services/user.service.js
@@ -1,22 +1,48 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-
 const getUserBoard = () => {
-  console.log("getUserBoard")
-  const user = JSON.parse(localStorage.getItem('user'));
-    if (user && user.body.token) {
-      console.log('authHeader() has the token >>>>>>>>>>  ' + user.body.token)
-      return axios.post("http://localhost:3001/api/v1/user/profile", 
-      {
-        Headers: { authorization: 'Bearer ' + user.body.token}
-      }
-      ); 
-    } else {
-      return {};
-    }
-}
-const UserService = {
-  getUserBoard
+  console.log("getUserBoard has been called");
+  console.log("authHeader: ", authHeader());
+  return axios({
+    method: "POST",
+    url: "http://localhost:3001/api/v1/user/profile",
+    headers: authHeader(),
+  });
 };
-export default UserService
+
+// const editUserBoard = () => {
+//   console.log("editUserBoard has been called");
+//   return axios({
+//     method: "PUT",
+//     url: "http://localhost:3001/api/v1/user/profile",
+//     headers: authHeader(),
+//     body: editHeader(),
+//   });
+// };
+const userService = { getUserBoard};
+export default userService;
+
+// const getUserBoard = async () => {
+//   console.log("getUserBoard")
+//   let mounted= true
+//   console.log( JSON.parse(localStorage.getItem('user')).body.token)
+//   const bearerToken = JSON.parse(localStorage.getItem("user")).body.token;
+//   try{
+//     const reponse = await axios.post("http://localhost:3001/api/v1/user/profile",
+//       {
+//         headers: {Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYjU0ZmU3YzE0ODdhOWJiYmNkY2NkNiIsImlhdCI6MTY1Njk0NzM0NCwiZXhwIjoxNjU3MDMzNzQ0fQ.GfubLo_HMHoxpeMFL_OZpmiNC7bNJRRs-kOx3F5Rea0'}
+//       })
+//     console.log("reponse>>>>>",reponse)
+//     if(mounted){
+//       const dataUser = reponse
+//       return dataUser
+//     }
+
+//   } catch{
+//     return null
+//   }}
+// const UserService = {
+//   getUserBoard
+// };
+// export default UserService
