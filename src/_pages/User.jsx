c40390d3cadf8fbba6c6668b766f3707d087eca5
LilouCode <lisa.music@outlook.com>
2022-07-09 09:07:24 +0200

    feat (user): add editing features

--- src/_pages/User.jsx
+++ src/_pages/User.jsx
@@ -1,13 +1,20 @@
-import React,  { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Account from "../_components/Account";
 import UserService from "../_services/user.service";
+import { Navigate } from "react-router-dom";
+import {toggleEdition} from "../_slices/user"
+import Edition from "../_components/Edition";
 
 const User = () => {
+  const dispatch = useDispatch();
+  const {isEditing} = useSelector( (state) => state.user);
   const [content, setContent] = useState("");
   useEffect(() => {
     UserService.getUserBoard().then(
       (response) => {
-        setContent(response.data);
+        setContent(response.data.body);
+        console.log(content);
       },
       (error) => {
         const _content =
@@ -20,16 +27,28 @@ const User = () => {
       }
     );
   }, []);
-  return (
+  const { user: currentUser } = useSelector((state) => state.auth);
+  return !currentUser ? (
+    <Navigate replace to={"/login"} />
+  ) : (
     <main className="main bg-dark">
-      <div className="header">
-        <h1>
-          Welcome back
-          <br />
-          {content}!
-        </h1>
-        <button className="edit-button">Edit Name</button>
-      </div>
+      {!isEditing ? (
+        <div className="header">
+          {" "}
+          <h1>
+            Welcome back
+            <br />
+            {content.firstName} {content.lastName}!
+          </h1>
+          <button className="edit-button" onClick={ () => dispatch(toggleEdition())}>Edit Name</button>
+        </div>
+      ): (
+        <div className="header">
+          {" "}
+          <h1>Welcome back</h1>
+          <Edition/>
+        </div>
+      )} 
       <h2 className="sr-only">Accounts</h2>
       <Account
         title="Argent Bank Checking (x8349)"
@@ -48,5 +67,5 @@ const User = () => {
       />
     </main>
   );
-}
+};
 export default User;
