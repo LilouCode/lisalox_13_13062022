fdba5b21cc310aa80e8475c57f6b9284aeacea2f
LilouCode <lisa.music@outlook.com>
2022-07-09 09:06:14 +0200

    feat: update login with remembered checkbox

--- src/_components/Login.jsx
+++ src/_components/Login.jsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect  } from "react";
+import React, {useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Navigate} from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { login } from "../_slices/auth";
 import { clearMessage } from "../_slices/message";
+import { toggleRemember, chargeToken } from "../_slices/user";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
@@ -10,9 +11,10 @@ const Login = (props) => {
   const [loading, setLoading] = useState(false);
   const { isLoggedIn } = useSelector((state) => state.auth);
   const { message } = useSelector((state) => state.message);
+  const { wantToBeRemembered } = useSelector((state) => state.user);
   const dispatch = useDispatch();
 
-  useEffect(()=> {
+  useEffect(() => {
     dispatch(clearMessage());
   }, [dispatch]);
 
@@ -31,59 +33,67 @@ const Login = (props) => {
     setLoading(true);
     dispatch(login({ email, password }))
       .unwrap()
-      .then(() => {
+      .then((response) => {
+        dispatch(chargeToken(response.user.body.token));
         props.history.push("/user");
         window.location.reload();
+        if (wantToBeRemembered) {
+          localStorage.setItem("user", JSON.stringify(response.data));
+        }
       })
       .catch(() => {
         setLoading(false);
       });
   };
-  
+
   if (isLoggedIn) {
     return <Navigate replace to="/user" />;
   }
 
   return (
-  <div>
+    <div>
       <div>
         <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={handleLogin}
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleLogin}
         >
-        <Form>
-      <div className="input-wrapper">
-        <label htmlFor="email">Email</label>
-        <Field name="email" type="text"/>
-        <ErrorMessage name="email" component="div"/>
-      </div>
+          <Form>
+            <div className="input-wrapper">
+              <label htmlFor="email">Email</label>
+              <Field name="email" type="text" />
+              <ErrorMessage name="email" component="div" />
+            </div>
 
-      <div className="input-wrapper">
-        <label htmlFor="password">Password</label>
-        <Field name="password" type="password"/>
-        <ErrorMessage name="password" component="div"/>
-      </div>
+            <div className="input-wrapper">
+              <label htmlFor="password">Password</label>
+              <Field name="password" type="password" />
+              <ErrorMessage name="password" component="div" />
+            </div>
 
-      <div className="input-remember">
-        <label>
-          <input type="checkbox" id="remember-me" />
-          Remember me
-        </label>
-      </div>
+            <div className="input-remember">
+              <label>
+                <input
+                  type="checkbox"
+                  id="remember-me"
+                  onClick={() => dispatch(toggleRemember())}
+                />
+                Remember me
+              </label>
+            </div>
 
-      <button type="submit" className="sign-in-button" disabled={loading}>Sign In</button>
-    </Form>
+            <button type="submit" className="sign-in-button" disabled={loading}>
+              Sign In
+            </button>
+          </Form>
         </Formik>
       </div>
-    {message && (
-      <div> 
-        <div role="alert">
-          {message}
+      {message && (
+        <div>
+          <div role="alert">{message}</div>
         </div>
-      </div>
-    )}
-  </div>
+      )}
+    </div>
   );
-}
+};
 export default Login;
