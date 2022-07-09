273c3822905105151343556d2118c3957bda357b
LilouCode <lisa.music@outlook.com>
2022-07-09 08:52:09 +0200

    refactor ( getUser ): adjust syntax

--- src/_services/auth-header.js
+++ src/_services/auth-header.js
@@ -2,7 +2,7 @@ export default function authHeader() {
     const user = JSON.parse(localStorage.getItem('user'));
     if (user && user.body.token) {
       console.log('authHeader() has the token >>>>>>>>>>  ' + user.body.token)
-      return { authorization: 'Bearer ' + user.body.token};
+      return { Authorization: `Bearer ` + user.body.token};
     } else {
       return {};
     }
